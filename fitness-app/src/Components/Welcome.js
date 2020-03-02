abf0d56bf8a442971aeedec497a9a8e5e8a5823d
Reagan Barrington <reagan@barringtonfamily.net>
2020-03-02 17:19:38 -0500

    Routes/Links working for ClassList and Welcome

--- fitness-app/src/Components/Welcome.js
+++ fitness-app/src/Components/Welcome.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import ClassList from "./ClassList";
 
 function Welcome() {
     return (
         <div className="welcome-container">
-            <h1>Welcome Component</h1>
+            <h1>Welcome to Anytime Fitness!</h1>
+            <h4>Our mission is to make fitness easily available to you, no matter your location or schedule.</h4>
+            <Link to="/classes">
+                <div>
+                    <button>Click Here to See Our Class List!</button>
+                </div>
+            </Link>
         </div>
     )
 }
