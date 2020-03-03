9b03c823c24bd149245f85702ec158c69bd7d903
Reagan Barrington <reagan@barringtonfamily.net>
2020-03-03 12:56:59 -0500

    padding fixed

--- fitness-app/src/Components/Welcome.js
+++ fitness-app/src/Components/Welcome.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import ClassList from "./ClassList";
+import "./Components.css";
 
 function Welcome() {
     return (
         <div className="welcome-container">
+            <img src="https://images.unsplash.com/photo-1552196563-55cd4e45efb3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1526&q=80"></img>
             <h1>Welcome to Anywhere Fitness!</h1>
             <h4>Our mission is to make fitness easily available to you, no matter your location or schedule.</h4>
             <Link to="/classes">
